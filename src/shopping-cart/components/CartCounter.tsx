8928ff3c565150fee393af60a952f479387c694c
Gerardo Miranda <gerardodev25@gmail.com>
2024-07-12 09:28:43 -0400

    feat(dev): :sparkles: implementing count redux axtions addOne and subtractOne

--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -1,26 +1,28 @@
 'use client';
-import { useState } from 'react';
+import { useAppDispatch, useAppSelector } from '@/store';
+import { addOne, subtractOne } from '@/store/counter/counterSlice';
 
 interface Props {
   value?: number;
 }
 
 export const CartCounter = ({ value = 0 }: Props) => {
-  const [counter, setCounter] = useState<number>(value);
+  const count = useAppSelector((state) => state.counter.count);
+  const dispatch = useAppDispatch();
   return (
     <>
-      <span className='text-9xl'>{counter}</span>
+      <span className='text-9xl'>{count}</span>
       <div className='flex'>
         <button
           className='flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2'
-          onClick={() => setCounter(counter + 1)}
+          onClick={() => dispatch(addOne())}
         >
           +1
         </button>
         <button
           className='flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2'
-          onClick={() => setCounter(counter - 1)}
-          disabled={counter <= 0}
+          onClick={() => dispatch(subtractOne())}
+          disabled={count <= 0}
         >
           -1
         </button>
